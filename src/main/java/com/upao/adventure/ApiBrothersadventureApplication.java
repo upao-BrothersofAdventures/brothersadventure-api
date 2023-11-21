5a309b4f04a7b204f828fbfba37f744ae46fab19
Ander <alopezb3@upao.edu.pe>
2023-11-20 22:40:06 -0500

    Nuevo api

---
+++ src/main/java/com/upao/adventure/ApiBrothersadventureApplication.java
@@ -0,0 +1,13 @@
+package com.upao.adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBrothersadventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBrothersadventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upao/adventure/ApiBrothersadventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBrothersadventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
